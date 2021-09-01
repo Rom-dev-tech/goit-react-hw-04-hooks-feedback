25071b28e380d76fa0d803f820be5ab872de542b
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-09-01 17:08:23 +0300

    Rewrites to hooks

--- src/UI/Section/Section.jsx
+++ src/UI/Section/Section.jsx
@@ -1,22 +1,16 @@
 import PropTypes from 'prop-types';
 import Container from '../../UI/Container';
-import Title from '../../components/Title';
 import s from '../Section/Section.module.scss';
 
-const Section = ({ title, titleSize, children }) => {
+const Section = ({ children }) => {
   return (
     <section className={s.section}>
-      <Container>
-        <Title title={title} titleSize={titleSize} />
-        {children}
-      </Container>
+      <Container>{children}</Container>
     </section>
   );
 };
 
 Section.propTypes = {
-  title: PropTypes.string.isRequired,
-  titleSize: PropTypes.string.isRequired,
   children: PropTypes.node,
 };
 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,70 +1,76 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Section from '../../UI/Section';
 import Statistics from '../Statistics';
 import FeedbackOptions from '../FeedbackOptions';
 import Notification from '../Notification';
+import Title from '../Title';
 
-class App extends Component {
-  static defaultProps = {
-    goodInitialValue: 0,
-    neutraInitiallValue: 0,
-    badInitialValue: 0,
+const App = ({
+  goodInitialValue = 0,
+  neutraInitiallValue = 0,
+  badInitialValue = 0,
+}) => {
+  const [good, setGood] = useState(goodInitialValue);
+  const [neutral, setNeutral] = useState(neutraInitiallValue);
+  const [bad, setBad] = useState(badInitialValue);
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  state = {
-    good: this.props.goodInitialValue,
-    neutral: this.props.neutraInitiallValue,
-    bad: this.props.badInitialValue,
-  };
-
-  countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad;
-  }
-
-  countPositiveFeedbackPercentage() {
-    if (this.countTotalFeedback() === 0) {
+  const countPositiveFeedbackPercentage = () => {
+    if (countTotalFeedback() === 0) {
       return 0;
     }
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-  }
-
-  onLeaveFeedback = value => {
-    this.setState(prevState => ({
-      [value]: prevState[value] + 1,
-    }));
+    return Math.round((good / countTotalFeedback()) * 100);
   };
 
-  render() {
-    const statsLabel = Object.keys(this.state);
-    const onLeaveFeedback = this.onLeaveFeedback;
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    return (
-      <>
-        <Section title="Please leave feedBack" titleSize="h1">
-          <FeedbackOptions
-            options={statsLabel}
-            onLeaveFeedback={onLeaveFeedback}
-          />
-        </Section>
+  const onLeaveFeedback = value => {
+    switch (value) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
 
-        <Section title="Statistics" titleSize="h2">
-          {total ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notification message="No feedback given..." />
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  const statsLabelArray = ['good', 'neutral', 'bad'];
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+  return (
+    <>
+      <Section>
+        <Title title="Please leave feedBack" titleSize="h1" />
+        <FeedbackOptions
+          options={statsLabelArray}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+
+        <Title title="Statistics" titleSize="h2" />
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="No feedback given..." />
+        )}
+      </Section>
+    </>
+  );
+};
 
 export default App;
